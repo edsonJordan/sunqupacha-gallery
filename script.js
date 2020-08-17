92ba34bc77a6616c2e7a6ad81f36e5b0432f47e0
edsonJordan <edson_4555@hotmail.com>
2020-08-17 16:49:55 -0500

    mejorando la logica del corazon

--- script.js
+++ script.js
@@ -16,14 +16,19 @@ const autoAdd = () =>{
         }
     autoAdd()
     let touch=0;  
+    
     const gallery = document.getElementById('gallery__photo');
+    gallery?.addEventListener('mousemove', (e)=>{
+      
+    })
     gallery?.addEventListener('click', (e)=>{            
+      
       touch++     
       if(touch == 1){        
       var time = setTimeout(function(){ touch=0}, 400);          
-      }else {              
-        const cora = document.getElementById("cora");
-        
+      }else {             
+      
+        const cora = document.getElementById("cora");        
         cora.style.left =JSON.stringify(e.pageX) + "px";                                              
         cora.style.top = JSON.stringify(e.pageY) + "px";                                              
     }           
