303b97321b1955da95b42bc16b9c63acc8e4a2f2
edsonJordan <edson_4555@hotmail.com>
2020-08-18 01:36:20 -0500

    finalizando animacion de latido de corazòn

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ const autoAdd = () =>{
           })                      
         }
     autoAdd()
+
+    /* End animation */
     const cora = document.getElementById("cora"); 
     cora?.addEventListener('animationend', (e)=> {      
       cora.classList.add("none");     
+      cora.style.animation ='';
     })
     
-    
+    /* Animation */
     let touch=0;      
     const gallery = document.getElementById('gallery__photo');
     gallery?.addEventListener('mousemove', (e)=>{
@@ -33,7 +36,21 @@ const autoAdd = () =>{
       var time = setTimeout(function(){ touch=0}, 400);          
       }else {                     
         const cora = document.getElementById("cora"); 
-        cora?.classList.toggle('none')       
+        //cora?.classList.add('anima_center')//throb
+        //cora.style.animation ='throb-left .6s';
+        let thRandom= Math.round(Math.random()*(3-1)+1);
+        switch (thRandom) {
+          case 1:
+            cora.style.animation ='throb-left .6s';
+            break;
+          case 2:
+            cora.style.animation ='throb-center .6s';
+            break;
+          case 3:
+            cora.style.animation ='throb-right .6s';
+            break;         
+        }       
+        cora?.classList.toggle('none')               
         cora.style.left =JSON.stringify((e.pageX)-25) + "px";                                              
         cora.style.top = JSON.stringify((e.pageY)-25) + "px";                                              
     }           
