0928ff7232d3ea33417e9330e092baed39b208f5
edsonJordan <edson_4555@hotmail.com>
2020-08-18 22:33:48 -0500

    voviendo

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const rNum =  new Random();
 
 
 const getImage = () =>{  
+<<<<<<< HEAD
     let pagRandom= Math.round(Math.random()*(34-1)+1);    
     /* ${pagRandom} */    
     const pag = [];
@@ -29,6 +30,23 @@ const getImage = () =>{
     /* content because repeat  */       
     }                                            
           
+=======
+    fetch('https://rickandmortyapi.com/api/character/?page='+pag)
+        .then(res => res.ok ? Promise.resolve(res): Promise.reject(res)) 
+        .then(res=> res.json())
+        .then(res => {           
+          const image = document.getElementById('gallery__photo');                                             
+          const fragment = document.createDocumentFragment();                            
+          for(const data of res.results){
+           const newImage = document.createElement('img')
+              newImage.setAttribute('src', data.image)
+              newImage.classList.add('img_photo')
+              fragment.appendChild(newImage)                             
+                }                                                                
+              image?.appendChild(fragment)
+              setScroll(image);                      
+          })                      
+>>>>>>> parent of 3c8c18d... agregando style a imagenes seleccionadas
         }
       getImage()    
       const callback = (entries) =>{      
@@ -75,13 +93,8 @@ const setScroll = (image) =>{
           case 3:
             cora.style.animation ='throb-right .6s';
             break;         
-        }
-          //console.log(e.target.nodeName);
-            if(e.target.nodeName == "IMG"){
-              let imgSelect = document.getElementById(e.target.id);
-              imgSelect?.classList.toggle("img_like");
-            }            
-            cora?.classList.toggle('none')               
+        }       
+        cora?.classList.toggle('none')               
             cora.style.left =JSON.stringify((e.pageX)-25) + "px";                                              
             cora.style.top = JSON.stringify((e.pageY)-25) + "px";                                              
             }                   
